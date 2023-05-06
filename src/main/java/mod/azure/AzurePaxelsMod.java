55a58d7629b4fec8e2c3c57ca20b2881a746ab77
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-05-06 11:22:08 -0400

    Update AzurePaxelsMod.java

--- src/main/java/mod/azure/AzurePaxelsMod.java
+++ src/main/java/mod/azure/AzurePaxelsMod.java
@@ -27,12 +27,12 @@ public class AzurePaxelsMod implements ModInitializer {
 		GOLDEN_PAXEL = item("golden_paxel", new AzurePaxel(Tiers.GOLD, 7.0f));
 		DIAMOND_PAXEL = item("diamond_paxel", new AzurePaxel(Tiers.DIAMOND, 6.0f));
 		NETHERITE_PAXEL = item("netherite_paxel", new AzurePaxel(Tiers.NETHERITE, 6.0f));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(WOODEN_PAXEL));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(STONE_PAXEL));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(IRON_PAXEL));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(GOLDEN_PAXEL));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(DIAMOND_PAXEL));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(NETHERITE_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(WOODEN_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(STONE_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(IRON_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(GOLDEN_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(DIAMOND_PAXEL));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(NETHERITE_PAXEL));
 	}
 
 	static <T extends Item> T item(String id, T c) {
