13963d5fe4324106fb0190dab871dbb14280fc94
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-08-30 16:24:17 -0400

    Neo Port

--- src/main/java/mod/azure/AzurePaxelsMod.java
+++ src/main/java/mod/azure/AzurePaxelsMod.java
@@ -1,9 +1,5 @@
 package mod.azure;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
@@ -11,37 +7,44 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-public class AzurePaxelsMod implements ModInitializer {
+@Mod(AzurePaxelsMod.MODID)
+public class AzurePaxelsMod {
 
-	public static final String MODID = "azure-paxels";
-	public static AzurePaxel WOODEN_PAXEL;
-	public static AzurePaxel STONE_PAXEL;
-	public static AzurePaxel IRON_PAXEL;
-	public static AzurePaxel GOLDEN_PAXEL;
-	public static AzurePaxel DIAMOND_PAXEL;
-	public static AzurePaxel NETHERITE_PAXEL;
+	public static final String MODID = "azurepaxels";
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+	public static final RegistryObject<Item> WOODEN_PAXEL = ITEMS.register("wooden_paxel", () -> new AzurePaxel(Tiers.WOOD, 7.0f));
+	public static final RegistryObject<Item> STONE_PAXEL = ITEMS.register("stone_paxel", () -> new AzurePaxel(Tiers.STONE, 8.0f));
+	public static final RegistryObject<Item> IRON_PAXEL = ITEMS.register("iron_paxel", () -> new AzurePaxel(Tiers.IRON, 7.0f));
+	public static final RegistryObject<Item> GOLDEN_PAXEL = ITEMS.register("golden_paxel", () -> new AzurePaxel(Tiers.GOLD, 7.0f));
+	public static final RegistryObject<Item> DIAMOND_PAXEL = ITEMS.register("diamond_paxel", () -> new AzurePaxel(Tiers.DIAMOND, 6.0f));
+	public static final RegistryObject<Item> NETHERITE_PAXEL = ITEMS.register("netherite_paxel", () -> new AzurePaxel(Tiers.NETHERITE, 6.0f));
 	public static final TagKey<Block> PAXEL_BLOCKS = TagKey.create(Registries.BLOCK, AzurePaxelsMod.modResource("paxel_blocks"));
 
-	@Override
-	public void onInitialize() {
-		WOODEN_PAXEL = item("wooden_paxel", new AzurePaxel(Tiers.WOOD, 7.0f));
-		STONE_PAXEL = item("stone_paxel", new AzurePaxel(Tiers.STONE, 8.0f));
-		IRON_PAXEL = item("iron_paxel", new AzurePaxel(Tiers.IRON, 7.0f));
-		GOLDEN_PAXEL = item("golden_paxel", new AzurePaxel(Tiers.GOLD, 7.0f));
-		DIAMOND_PAXEL = item("diamond_paxel", new AzurePaxel(Tiers.DIAMOND, 6.0f));
-		NETHERITE_PAXEL = item("netherite_paxel", new AzurePaxel(Tiers.NETHERITE, 6.0f));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(WOODEN_PAXEL));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(STONE_PAXEL));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(IRON_PAXEL));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(GOLDEN_PAXEL));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(DIAMOND_PAXEL));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(NETHERITE_PAXEL));
+	public AzurePaxelsMod() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ITEMS.register(modEventBus);
+		MinecraftForge.EVENT_BUS.register(this);
+		modEventBus.addListener(this::addCreative);
 	}
 
-	static <T extends Item> T item(String id, T c) {
-		Registry.register(BuiltInRegistries.ITEM, AzurePaxelsMod.modResource(id), c);
-		return c;
+	private void addCreative(BuildCreativeModeTabContentsEvent event) {
+		if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+			event.accept(WOODEN_PAXEL);
+			event.accept(STONE_PAXEL);
+			event.accept(IRON_PAXEL);
+			event.accept(GOLDEN_PAXEL);
+			event.accept(DIAMOND_PAXEL);
+			event.accept(NETHERITE_PAXEL);
+		}
 	}
 
 	public static final ResourceLocation modResource(String name) {
