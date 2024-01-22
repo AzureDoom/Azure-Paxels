a3518a864845f97aca4db02290954311927229ec
Uko Kokņevičs <perkontevs@gmail.com>
2024-01-22 03:11:31 +0200

    Ask minecraft directly for the axe & shovel maps.

--- src/main/java/mod/azure/AzurePaxel.java
+++ src/main/java/mod/azure/AzurePaxel.java
@@ -3,32 +3,26 @@ package mod.azure;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import com.google.common.collect.ImmutableMap.Builder;
-import com.google.common.collect.Maps;
-
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class AzurePaxel extends DiggerItem {
-
-	protected static final Map<Block, BlockState> SHOVEL_LOOKUP = Maps.newHashMap(new Builder<Block, BlockState>().put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH.defaultBlockState()).put(Blocks.DIRT, Blocks.DIRT_PATH.defaultBlockState()).put(Blocks.PODZOL, Blocks.DIRT_PATH.defaultBlockState()).put(Blocks.COARSE_DIRT, Blocks.DIRT_PATH.defaultBlockState()).put(Blocks.MYCELIUM, Blocks.DIRT_PATH.defaultBlockState()).put(Blocks.ROOTED_DIRT, Blocks.DIRT_PATH.defaultBlockState()).build());
-
-	protected static final Map<Block, Block> BLOCK_STRIPPING_MAP = new Builder<Block, Block>().put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD).put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG).put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD).put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG).put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD).put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG).put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD)
-			.put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG).put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD).put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG).put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD).put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG).put(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM).put(Blocks.WARPED_HYPHAE, Blocks.STRIPPED_WARPED_HYPHAE).put(Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM).put(Blocks.CRIMSON_HYPHAE, Blocks.STRIPPED_CRIMSON_HYPHAE)
-			.build();
+	protected static final Map<Block, BlockState> SHOVEL_LOOKUP = Shovel.getFlattenables();
+	protected static final Map<Block, Block> BLOCK_STRIPPING_MAP = Axe.getStrippables();
 
 	public AzurePaxel(Tier tier, Float damage) {
 		super(damage, -2.8f, tier, AzurePaxelsMod.PAXEL_BLOCKS, new Item.Properties().stacksTo(1));
@@ -68,5 +62,25 @@ public class AzurePaxel extends DiggerItem {
 				});
 		}
 		return InteractionResult.SUCCESS;
+	}
+
+	private static final class Axe extends AxeItem {
+		public static Map<Block, Block> getStrippables() {
+			return AxeItem.STRIPPABLES;
+		}
+
+		private Axe(Tier tier, float f, float g, Properties properties) {
+			super(tier, f, g, properties);
+		}
+	}
+
+	private static final class Shovel extends ShovelItem {
+		public static Map<Block, BlockState> getFlattenables() {
+			return ShovelItem.FLATTENABLES;
+		}
+
+		private Shovel(Tier tier, float f, float g, Properties properties) {
+			super(tier, f, g, properties);
+		}
 	}
 }
